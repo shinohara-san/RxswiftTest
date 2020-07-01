d6a46c3bb2e00aa4beed818e0cf1d3bca9ea0a2c
shinohara-san <someiyoshino0809@gmail.com>
2020-07-02 08:33:50 +0900

    first

---
+++ RxViewControllerCommunication/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  RxViewControllerCommunication
+//
+//  Created by Brian Advent on 09.01.18.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+//protocol CharacterDelegate {
+//    func didSelectCharacter (_ name:String)
+//}
+//
+class DetailViewController: UIViewController {
+   
+    
+//    var delegate:CharacterDelegate?
+    
+    private let selectedCharacterVar = Variable("User")
+    var selectedCharacter: Observable<String> {
+        return selectedCharacterVar.asObservable()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func characterSelected(_ sender: UIButton) {
+        guard let characterName = sender.titleLabel?.text else {return}
+        selectedCharacterVar.value = characterName
+//        if let delegateObject = delegate {
+//            delegateObject.didSelectCharacter(characterName)
+//        }
+        
+    }
+    
+    
+}
